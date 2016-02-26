ecd5e69f5b1fadff11dd52020636e2bf5686139c
Dan Huang <dhuang28@horizon.csueastbay.edu>
2016-02-25 21:34:46 -0800

    the GUI now shows after program start. updated menubar.

--- WesternRaceLogger/src/westernracelogger/controller/WesternRaceLogger.java
+++ WesternRaceLogger/src/westernracelogger/controller/WesternRaceLogger.java
@@ -1,15 +1,72 @@
 package westernracelogger.controller;
 
-public class WesternRaceLogger {
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stubsdfghjk
-		//test test test hi
-		//Test2 from Josh
-		//Test commit from Yuki
-		//Dammn daniel
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import westernracelogger.model.Runner;
+
+public class WesternRaceLogger extends Application {
+
+	
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+    private ObservableList<Runner> runnerData = FXCollections.observableArrayList();
+
+    
+    
+	@Override
+    public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("WSER application");
 		
-		System.out.println("test");
-	}
+		initRootLayout();
+		showRaceView();
+    }
+	
+	public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(WesternRaceLogger.class.getResource("/westernracelogger/view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            //primaryStage.setResizable(false);
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public void showRaceView() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(WesternRaceLogger.class.getResource("/westernracelogger/view/RaceView.fxml"));
+            AnchorPane RaceView = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(RaceView);
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public static void main(String[] args) {
+        launch(args);
+    }
 
 }
