6f65e2ffd1ae470e9425191b069ae64ea6095ce7
Xavier G <Xavier@JavierMobile>
2016-03-03 19:55:46 -0800

    //admin upload into db function integrated

--- WesternRaceLogger/src/westernracelogger/model/SQLDriver.java
+++ WesternRaceLogger/src/westernracelogger/model/SQLDriver.java
@@ -30,12 +30,21 @@ public class SQLDriver {
 			java.util.Date date = new java.util.Date(); // Right now
 			java.sql.Timestamp timestamp = new java.sql.Timestamp(date.getTime());
 			Runner runner= new Runner();
-			runner.set_age(13); runner.set_first_name("Adolf"); runner.set_last_name("Loaf");
-			runner.set_gender("F"); runner.set_in_time(date);
-			//runner.set_runner_id("A344");
 			
-			runners.add(runner);
+			runner.set_age(13); 
+	        runner.set_name("Adolf Loaf");
+			runner.set_gender("F");
+			runner.set_in_time(timestamp);
+		    runner.set_in_place(11);
+			runner.set_out_place(12);
+			runner.set_out_time(timestamp);
+			runner.set_runner_id("A374");
+			
+			//runners.add(runner);
+			
 			UpdateTableIn(runner);
+			
+			UploadIntoTable(runner);
 			PrintTable();
 			
 			
@@ -51,10 +60,42 @@ public class SQLDriver {
 
 	}
 	
+	
+	/*
+	 * Read from file and upload to SQLDB (admin only)
+	 */
+	public static void UploadIntoTable(Runner runner){
+		
+		//Runner runner;
+		
+		String query = "INSERT INTO runners(r_id, r_name, r_placein, r_placeout, r_timein, r_timeout, r_age, r_gender)"
+				+ " VALUES ('" + runner.get_runner_id().getValue() + "','" + runner.get_name().getValue() + "' ," +
+				runner.get_in_place().getValue() + "," + runner.get_out_place().getValue() + ",'" +
+				runner.get_in_time() + "','" + runner.get_out_time() + "'," + runner.get_age().getValue() + 
+				",'" + runner.get_gender().getValue()
+				+"');";
+		try {
+			
+			Statement stmt = conn.createStatement();
+			int ret = stmt.executeUpdate(query);
+			
+			
+			
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+	
+	
 	/*
 	 * Read rows form SQL database and put into linked list
 	 */
 	public static LinkedList<Runner> RetrieveTableData(){  
+		
+		LinkedList<Runner> retrieved = new LinkedList<Runner>();
 		
 		String query= "Select * from runners;";
 		try {
@@ -65,13 +106,13 @@ public class SQLDriver {
 			while(rs.next()){
 				Runner runner = new Runner();
 				runner.set_age(rs.getInt("r_age"));
-				runner.set_first_name(rs.getString("r_name"));
+				runner.set_name(rs.getString("r_name"));
 				runner.set_gender(rs.getString("r_gender"));
-				//runner.set_runner_id(rs.getString("r_ID"));
+				runner.set_runner_id(rs.getString("r_ID"));
 				runner.set_in_place(rs.getInt("r_placein"));
 				runner.set_out_place(rs.getInt("r_placeout"));
-				runner.set_out_time(rs.getDate("r_timeout"));
-				runner.set_in_time(rs.getDate("r_timein"));
+				runner.set_out_time(rs.getTimestamp("r_timeout"));
+				runner.set_in_time(rs.getTimestamp("r_timein"));
 				runners.add(runner);	//add runner to list, for retunr at end of function
 			}
 			
@@ -80,7 +121,7 @@ public class SQLDriver {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-		return runners;
+		return retrieved;
 		
 		
 	}
@@ -90,11 +131,13 @@ public class SQLDriver {
 	 */
 	public static void UpdateTableIn(Runner runner){
 		
+		System.out.println(runner.get_in_time());
+		System.out.println(runner.get_runner_id().getValue());
 		String query = "UPDATE runners"
-				+ " SET r_placein= "+runner.in_place
-				+ ", r_timein= '" + runner.in_time +"'"
+				+ " SET r_placein= "+runner.get_in_place().getValue()
+				+ ", r_timein= '" + runner.get_in_time() +"'"
 				
-				+ " WHERE r_id=" + "'"+runner.runner_id+"'" +";" ;
+				+ " WHERE r_id=" + "'"+runner.get_runner_id().getValue()+"';" ;
 		try {
 			
 			Statement stmt = conn.createStatement();
@@ -116,9 +159,9 @@ public class SQLDriver {
 	public static void UpdateTableOut(Runner runner){
 		
 		String query = "UPDATE runners"
-				+ " SET r_placeout= "+runner.out_place
-				+ ", r_timeout= '" + runner.out_time +"'"
-				+ " WHERE r_id=" + "'"+runner.runner_id+"'" +";" ;
+				+ " SET r_placeout= "+runner.get_out_place().intValue()
+				+ ", r_timeout= '" + runner.get_out_time()+"'"
+				+ " WHERE r_id=" + "'"+runner.get_runner_id().getValue()+"'" +";" ;
 		try {
 			
 			Statement stmt = conn.createStatement();
@@ -146,7 +189,10 @@ public class SQLDriver {
 			ResultSet rs= stmt.executeQuery(query);
 			
 			while(rs.next()){
-				System.out.println("ID: "+ rs.getString("r_id") +"| Name: " + rs.getString("r_name") + "| PlaceIn: " + rs.getString("r_placein") + "| PlaceOut: " + rs.getString("r_placeout"));
+				System.out.println("ID: "+ rs.getString("r_id") +"| Name: " + rs.getString("r_name") 
+				+ "| PlaceIn: " + rs.getString("r_placein") + "| PlaceOut: " 
+				+ rs.getString("r_placeout") + "| TimeIn: " +rs.getTimestamp("r_timein")
+				+ "| TimeOut: " +rs.getTimestamp("r_timeout"));
 				
 			}
 			
