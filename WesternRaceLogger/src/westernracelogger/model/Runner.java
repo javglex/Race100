6e4b9d8db42f9d47af2efdce36f14aeb701fcbb1
Xavier G <Xavier@JavierMobile>
2016-03-03 18:06:01 -0800

    //merged first and last name into one "name"

--- WesternRaceLogger/src/westernracelogger/model/Runner.java
+++ WesternRaceLogger/src/westernracelogger/model/Runner.java
@@ -12,50 +12,45 @@ public class Runner {
 	
 	IntegerProperty in_place;
 	IntegerProperty out_place;
-	IntegerProperty runner_id;
+	StringProperty runner_id;
 	Date in_time;
 	Date out_time;
-	StringProperty first_name;
-	StringProperty last_name;
+	StringProperty name;
 	StringProperty gender;
 	IntegerProperty age;
 	
 	//setters
 	public void set_in_place(int i){ in_place.setValue(i);}
 	public void set_out_place(int i){ out_place.setValue(i);}
-	public void set_runner_id(int i){ runner_id.setValue(i);}
+	public void set_runner_id(String i){ runner_id.setValue(i);}
 	public void set_in_time(Date d){ in_time = d; }
 	public void set_out_time(Date d){ out_time = d; }
-	public void set_first_name(String s){ first_name.setValue(s);}
-	public void set_last_name(String s){ last_name.setValue(s); }
+	public void set_name(String s){ name.setValue(s);}
 	public void set_gender(String s){ gender.setValue(s); }
 	public void set_age(int i){ age.setValue(i);}
 	
 	//getters
 	public IntegerProperty get_in_place(){ return in_place; }
 	public IntegerProperty get_out_place(){ return out_place; }
-	public IntegerProperty get_runner_id(){ return runner_id; }
+	public StringProperty get_runner_id(){ return runner_id; }
 	public Date get_in_time(){ return in_time; }
 	public Date get_out_time(){ return out_time; }
-	public StringProperty get_first_name(){ return first_name; }
-	public StringProperty get_last_name(){ return last_name; }
+	public StringProperty get_name(){ return name; }
 	public StringProperty get_gender(){ return gender; }
 	public IntegerProperty get_age(){ return age; };
 	
 	public Runner(){
-		this.runner_id = new SimpleIntegerProperty(0);
-		this.first_name = new SimpleStringProperty("") ;
-		this.last_name = new SimpleStringProperty("");
+		this.runner_id = new SimpleStringProperty("");
+		this.name = new SimpleStringProperty("") ;
 		this.gender = new SimpleStringProperty("");
 		this.age = new SimpleIntegerProperty(0);
 	}
 	
-	public Runner(int runner_id, String first_name, String last_name, String gender, int age ) {
+	public Runner(String runner_id, String name, String gender, int age ) {
 		
 		
-		this.runner_id = new SimpleIntegerProperty(runner_id);
-		this.first_name = new SimpleStringProperty(first_name) ;
-		this.last_name = new SimpleStringProperty(last_name);
+		this.runner_id = new SimpleStringProperty(runner_id);
+		this.name = new SimpleStringProperty(name);
 		this.gender = new SimpleStringProperty(gender);
 		this.age = new SimpleIntegerProperty(age);
     }
